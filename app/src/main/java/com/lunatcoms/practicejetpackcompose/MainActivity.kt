3ca068d8400e4b36b10742ec5cd4c8810ae94cd6
leolunaf25 <lunalunaf25@gmail.com>
2024-12-19 23:39:29 -0600

    compose introduction

--- app/src/main/java/com/lunatcoms/practicejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/lunatcoms/practicejetpackcompose/MainActivity.kt
@@ -4,29 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.lunatcoms.practicejetpackcompose.ui.theme.PracticeJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PracticeJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            helloApp()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun helloApp(){
+    PracticeJetpackComposeTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun exampleModifier() {
+    Text(
+        text = "Primer prueba",
+        modifier = Modifier.padding(horizontal = 16.dp)
+    )
 }
 
 @Composable
@@ -41,6 +54,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PracticeJetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Horoscope")
+    }
+}
+
+@Preview(showBackground = true, name = "Prueba")
+@Composable
+fun SecondPreview() {
+    PracticeJetpackComposeTheme {
+        Greeting("Horoscope")
     }
 }
