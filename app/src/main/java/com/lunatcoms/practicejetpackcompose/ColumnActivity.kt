d0c9e24db254b2fef03e3da3fde2c3beefcbe1fa
leolunaf25 <lunalunaf25@gmail.com>
2025-01-02 23:07:42 -0600

    update manifest lauch

--- app/src/main/java/com/lunatcoms/practicejetpackcompose/ColumnActivity.kt
+++ app/src/main/java/com/lunatcoms/practicejetpackcompose/ColumnActivity.kt
@@ -7,12 +7,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class ColumnActivity : ComponentActivity() {
@@ -27,18 +32,27 @@ class ColumnActivity : ComponentActivity() {
 @Preview
 @Composable
 fun app() {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Red)
     ) {
+        item{
         Image(
+            modifier = Modifier.fillMaxWidth().height(400.dp).background(Color.Blue),
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Logo"
+            contentDescription = "Logo",
+
         )
-        Text(text = "Hola mundo 1", fontSize = 32.sp, color = Color.White)
-        Text(text = "Hola mundo 2")
-        Text(text = "Hola mundo 3")
+        Text(
+            text = "Hola mundo 1",
+            fontSize = 32.sp,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(text = "Hola mundo 2", color = Color.White)
+        Text(text = "Hola mundo 3", color = Color.White)}
     }
 }
 
